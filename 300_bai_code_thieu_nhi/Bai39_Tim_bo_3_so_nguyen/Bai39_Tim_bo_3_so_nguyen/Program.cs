4d1c71950a7637fefd788cec117afbf92f672081
zizisn92 <hungnhi3392@gmail.com>
2021-09-01 22:12:06 +0700

    update

---
+++ 300_bai_code_thieu_nhi/Bai39_Tim_bo_3_so_nguyen/Bai39_Tim_bo_3_so_nguyen/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Bai39_Tim_bo_3_so_nguyen
+{
+    class Program
+    {
+        static void TimBoBaSoNguyen(int n)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    for (int k = 1; k < n; k++)
+                    {
+                        if(i*i+j*j==k*k)
+                        {
+                            if (i % 2 == 0 && j%2==0&& k%2==0 && (i == j - 2 && j == k - 2))
+                            {
+                                Console.WriteLine($"({i},{j},{k}):Ba so chan lien tiep");
+                            }
+                            if(i==j-1&&j==k-1)
+                            {
+                                Console.WriteLine($"({i},{j},{k}):Ba so nguyen lien tiep");
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            TimBoBaSoNguyen(100);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 300_bai_code_thieu_nhi/Bai40_Tim_cac_bo/Bai40_Tim_cac_bo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Bai40_Tim_cac_bo
+{
+    class Program
+    {
+        static void TimBoSo(int n)
+        {
+            for (int i = 0; i < n/5; i++)
+            {
+                for (int j = 0; j < n/3; j++)
+                {
+                    for (int k = 0; k < n*3; k++)
+                    {
+                       if(i*5+j*3+k/3==n)
+                       {
+                            Console.WriteLine($"({i},{j},{k})");
+                            break;
+                       }    
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TimBoSo(100);
+            Console.ReadKey();
+           // Console.WriteLine("Hello World!");
+        }
+    }
+}
